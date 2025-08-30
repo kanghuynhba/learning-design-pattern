e44d82f36c626b144abd594a336ed878b8b08c0d
kanghuynhba <kanghuynhba@gmail.com>
2025-08-30 15:17:23 +0700

    add first demo for prototype pattern

--- example/src/main/java/com/hbk/example/PrototypeDemo/demo_1/Shape.java
+++ example/src/main/java/com/hbk/example/PrototypeDemo/demo_1/Shape.java
@@ -1,5 +1,22 @@
 package PrototypeDemo.demo_1;
 
-import 
+import lombok.Data; 
 
-public class Shape implements Clonable
+@Data
+public abstract class Shape implements Cloneable {
+
+    private String id;
+    public String type;
+
+    abstract void draw();
+
+    public Object clone() {
+        Object clone = null;
+        try {
+            clone=super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    }
+}
