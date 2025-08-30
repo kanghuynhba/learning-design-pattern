758a85bf5c2c11240f785d76ecd2b098010e2655
kanghuynhba <kanghuynhba@gmail.com>
2025-08-30 16:05:38 +0700

    apply Prototype pattern by using clone's method for Entity

--- book_market/src/main/java/com/hbk/book_market/entity/Author.java
+++ book_market/src/main/java/com/hbk/book_market/entity/Author.java
@@ -3,7 +3,14 @@ package com.hbk.book_market.entity;
 import lombok.Data;
 
 @Data
-public class Author {
+public class Author implements Cloneable {
     private long id;
     private String name;
+
+    @Override
+    public Author clone() {
+        final Author clone=new Author();
+        clone.name=name;
+        return clone;
+    }
 }
