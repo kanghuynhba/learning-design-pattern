adb15e74cb07f85ebd3ea4be824e0fd0e6d3170a
kanghuynhba <kanghuynhba@gmail.com>
2025-09-03 21:04:40 +0700

    add second demo about login authentication process applying chain of responsibility

---
+++ examples/src/main/java/com/hbk/example/ChainOfResponsibilityDemo/demo_2/server/Server.java
@@ -0,0 +1,40 @@
+package ChainOfResponsibilityDemo.demo_2.server;
+
+import ChainOfResponsibilityDemo.demo_2.middleware.Middleware;
+
+import java.util.Map;
+import java.util.HashMap;
+
+// Server class.
+public class Server {
+    private Map<String, String> user=new HashMap<>();
+    private Middleware middleware;
+
+    // Client passes the chain of object to server.
+    // This improves flexibility and makes testing the server classes easier.
+    public void setMiddleware(Middleware middleware) {
+        this.middleware=middleware;
+    }
+
+    // Server gets email and password from client
+    // and sends the authorization request to the chain.
+    public boolean logIn(String email, String password) {
+        if(middleware.check(email, password)) {
+            System.out.println("Authorization has been successful!");
+            return true; 
+        }
+        return false;
+    }
+
+    public void register(String email, String password) {
+        user.put(email, password);
+    }
+
+    public boolean hasEmail(String email) {
+        return user.containsKey(email);
+    }
+
+    public boolean isValidPassword(String email, String password) {
+        return user.get(email).equals(password);
+    }
+}
