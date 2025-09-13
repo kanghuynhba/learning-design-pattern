2eab625187c65a63966cc94e7310e528233b2200
kanghuynhba <kanghuynhba@gmail.com>
2025-09-13 17:35:18 +0700

    add second demo to command pattern

---
+++ examples/src/main/java/com/hbk/example/CommandDemo/demo_2/Cook.java
@@ -0,0 +1,19 @@
+package com.hbk.example.CommandDemo.demo_2;
+
+public class Cook {
+    private int mTimeCook;
+    public Cook() {
+        this(10);
+    }
+    public Cook(int time) {
+        this.mTimeCook=time;
+    }
+    
+    public void boil() {
+        System.out.println("Boil in " + mTimeCook);
+    }
+
+    public void grill() {
+        System.out.println("Grill in " + mTimeCook);
+    }
+}
---
+++ examples/src/main/java/com/hbk/example/CommandDemo/demo_2/Waiter.java
@@ -0,0 +1,24 @@
+package com.hbk.example.CommandDemo.demo_2;
+
+import  com.hbk.example.CommandDemo.demo_2.orders.IOrder;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Waiter {
+    private Queue<IOrder> mOrders;
+    public Waiter() {
+        mOrders=new LinkedList<>();
+    }
+
+    public void takeOrder(IOrder order) {
+        mOrders.add(order);
+    }
+
+    public void processOrders() {
+        for(IOrder order:mOrders) {
+            order.execute();
+        }
+        mOrders.clear();
+    }
+}
